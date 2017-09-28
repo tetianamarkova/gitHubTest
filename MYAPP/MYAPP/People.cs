6bbcfaad2605af36b98ae9f25c4d9da58461d206
student <student@K202-05>
2017-09-28 20:01:20 +0300

    olya_1

--- MYAPP/MYAPP/People.cs
+++ MYAPP/MYAPP/People.cs
@@ -11,12 +11,7 @@ namespace MYAPP
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public void Add(){ }
-        public void Remove() { }
-        public void Sort() { }
-        public void Print() { }
-        public void Save_to_file() { }
-        public void Load_from_file() { }
+       
 
     }
 }
