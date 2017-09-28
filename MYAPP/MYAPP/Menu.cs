7c5baa1338edf116afdf95de9881e21790b85c89
student <student@K202-08>
2017-09-28 20:21:02 +0300

    Save and Load realisation

--- MYAPP/MYAPP/Menu.cs
+++ MYAPP/MYAPP/Menu.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Soap;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,30 @@ namespace MYAPP
 {
     class Menu
     {
+        List<People> people;
+        public Menu()
+        {
+            people = new List<People>();
+        }
+        public void SaveToFile()
+        {
+            using (FileStream fs =
+                new FileStream("PhoneBookXML.xml", FileMode.Create))
+            {
+                SoapFormatter xs = new SoapFormatter();
+                xs.Serialize(fs, people);
+            }
+            Console.WriteLine("Saved to PhoneBook");
+        }
+        public void LoadFromFile()
+        {
+            using (FileStream fs =
+                new FileStream("PhoneBookXML.xml", FileMode.Open))
+            {
+                SoapFormatter xs = new SoapFormatter();
+                people = (List<People>)xs.Deserialize(fs);
+            }
+            Console.WriteLine("Loaded from PhoneBook");
+        }
     }
 }
