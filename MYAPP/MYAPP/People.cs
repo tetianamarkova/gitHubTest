35348c70d4423fefa46be92e2ecc4296c6185a33
student <student@K202-05>
2017-09-28 19:57:19 +0300

    People is created

---
+++ MYAPP/MYAPP/People.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYAPP
+{
+    class People
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public void Add(){ }
+        public void Remove() { }
+        public void Sort() { }
+        public void Print() { }
+        public void Save_to_file() { }
+        public void Load_from_file() { }
+
+    }
+}
